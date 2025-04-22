ac80bc5dd2ad67b8ff265161ab0385768420495b
Laura Lufray <lauralufray98@gmail.com>
2025-04-22 11:11:03 +0200

    function changes

--- src/NewtonsMethod.jl
+++ src/NewtonsMethod.jl
@@ -3,32 +3,40 @@ module NewtonsMethod
 using LinearAlgebra, Statistics, Plots, ForwardDiff
 
 function newtonroot(f, f_prime, x_0; tolerance = 1E-7, maxiter = 1000)
-    # setup the algorithm
     x_old = x_0
     normdiff = Inf
     iter = 1
     while normdiff > tolerance && iter <= maxiter
-        x_new = x_old - f(x_old) / f_prime(x_old) # use the passed in map
+        x_new = x_old - f(x_old) / f_prime(x_old)
         normdiff = norm(x_new - x_old)
         x_old = x_new
-        iter = iter + 1
+        iter += 1
     end
-    return (; root = x_old, normdiff, iter) # A named tuple
+    return (; root = x_old, normdiff, iter)
 end
 
 function newtonroot(f, x_0; tolerance = 1E-7, maxiter = 1000)
-    # setup the algorithm
     x_old = x_0
     normdiff = Inf
     iter = 1
     while normdiff > tolerance && iter <= maxiter
         f_prime = ForwardDiff.derivative(f, x_old)
-        x_new = x_old - f(x_old) / f_prime 
+        x_new = x_old - f(x_old) / f_prime
         normdiff = norm(x_new - x_old)
         x_old = x_new
-        iter = iter + 1
+        iter += 1
     end
-    return (; root = x_old, normdiff, iter) # A named tuple
+    return (; root = x_old, normdiff, iter)
+end
+
+function newtonroot(f, x_0; f_prime::Function = nothing, tolerance = 1E-7, maxiter = 1000)
+    if f_prime === nothing
+        # Use automatic differentiation if f_prime is not provided
+        return newtonroot(f, x_0; tolerance, maxiter)
+    else
+        # Use the provided derivative function
+        return newtonroot(f, f_prime, x_0; tolerance, maxiter)
+    end
 end
 
 export newtonroot
