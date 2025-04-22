07641d9723d0f85ca13d45eabbac1e4f931a46aa
Laura Lufray <lauralufray98@gmail.com>
2025-04-22 11:01:35 +0200

    solve issues test

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,25 +5,25 @@ using Test
     # Test 1: Root of a quadratic function
     f(x) = x^2 - 4
     f_prime(x) = 2x
-    @test newtonroot(f, f_prime; x_0=1.0) ≈ 2.0
-    @test newtonroot(f, f_prime; x_0=-1.0) ≈ -2.0
+    @test newtonroot(f, f_prime; x_0=1.0).root ≈ 2.0
+    @test newtonroot(f, f_prime; x_0=-1.0).root ≈ -2.0
 
     # Test 2: Automatic differentiation
-    @test newtonroot(f; x_0=1.0) ≈ 2.0
-    @test newtonroot(f; x_0=-1.0) ≈ -2.0
+    @test newtonroot(f; x_0=1.0).root ≈ 2.0
+    @test newtonroot(f; x_0=-1.0).root ≈ -2.0
 
     # Test 3: Non-convergence for a function without a root
     g(x) = x^2 + 2
-    @test newtonroot(g, x -> 2x; x_0=1.0, maxiter=100) === nothing
+    @test newtonroot(g, x -> 2x; x_0=1.0, maxiter=100).normdiff > 1E-7
 
     # Test 4: BigFloat precision
     h(x) = BigFloat(x)^2 - BigFloat(4)
     h_prime(x) = 2 * BigFloat(x)
-    @test newtonroot(h, h_prime; x_0=BigFloat(1.0)) ≈ BigFloat(2.0)
+    @test newtonroot(h, h_prime; x_0=BigFloat(1.0)).root ≈ BigFloat(2.0)
 
     # Test 5: Max iterations
-    @test newtonroot(f, f_prime; x_0=1.0, maxiter=1) === nothing
+    @test newtonroot(f, f_prime; x_0=1.0, maxiter=1).iter == 2
 
     # Test 6: Tolerance
-    @test newtonroot(f, f_prime; x_0=1.0, tol=1e-1) ≈ 2.0
+    @test newtonroot(f, f_prime; x_0=1.0, tol=1e-1).root ≈ 2.0
 end
